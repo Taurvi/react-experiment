e844514c44d217f95c0097fce2049dc6a7710448
Stephen Rimbakusumo <srimbakusumo@gmail.com>
2016-07-11 19:17:01 -0700

    Completed "Hello World"

---
+++ HelloWorld/HelloWorldJSX.js
@@ -0,0 +1,7 @@
+// Calls ReactDOM to render
+ReactDOM.render(
+    // The HTML to render
+    <h1>Hello, world!</h1>,
+    // Locate element in HTML and render in it
+    document.getElementById('example')
+);
